43c41ce822cfd9334614459109b4131c9c0c7ac4
XeduR <eetu.rantanen@spyric.com>
2021-06-20 18:50:31 +0300

    Update spyricFontLoader.lua
    Updating Spyric Font Loader to its newest version.

--- app-source/spyricFontLoader.lua
+++ app-source/spyricFontLoader.lua
@@ -1,125 +1,116 @@
-local M = {}
+---------------------------------------------------------------------------
+--     _____                  _         ______                           --
+--    / ___/____  __  _______(_)____   / ____/___ _____ ___  ___  _____  --
+--    \__ \/ __ \/ / / / ___/ / ___/  / / __/ __ `/ __ `__ \/ _ \/ ___/  --
+--   ___/ / /_/ / /_/ / /  / / /__   / /_/ / /_/ / / / / / /  __(__  )   --
+--  /____/ .___/\__, /_/  /_/\___/   \____/\__,_/_/ /_/ /_/\___/____/    --
+--      /_/    /____/                                                    --
+--                                                                       --
+--  © 2020-2021 Spyric Games Ltd.             Last Updated: 21 May 2021  --
+---------------------------------------------------------------------------
+--  License: MIT                                                         --
+---------------------------------------------------------------------------
 
--- Require Lua File System.
+local fontLoader = {}
+
 local lfs = require( "lfs" )
 
 -- Localise global functions.
-local _type = type
-local _print = print
-local _getTimer = system.getTimer
+local type = type
+local print = print
 local newText = display.newText
-local remove = display.remove
+local dRemove = display.remove
 local pathForFile = system.pathForFile
-
-local title = "Spyric Font Loader - "
 
 --[[
 	In Solar2D, a font is cached when it is used for the first time during runtime. The speed of this process varies greatly across
 	operating systems and fonts. This function will locate all fonts in a given folder, and optionally within its subfolders, and
 	then this function will use the fonts once so that they are cached for faster use later.
 ]]--
-M.preload = function( folder, params )
-	local deepScan, consoleOutput, directory
-
-	-- Validate the folder and all optional parameters.
-	if _type( folder ) ~= "string" then
-		_print( "ERROR: " .. title .. "bad argument #1 to 'preload' (string expected, got " .. _type( folder ) .. ")" )
-		return
-	end
-
-	if _type( params ) == "table" then
-		if params.deepScan and _type( params.deepScan ) ~= "boolean" then
-			_print( "WARNING: " .. title .. "bad argument #2 'deepScan' to 'preload' (boolean expected, got " .. _type( params.deepScan ) .. ", defaulting to false)" )
-			deepScan = false
-		else
-			deepScan = params.deepScan or false
-		end
-
-		if params.consoleOutput and _type( params.consoleOutput ) ~= "boolean" then
-			_print( "WARNING: " .. title .. "bad argument #2 'consoleOutput' to 'preload' (boolean expected, got " .. _type( params.consoleOutput ) .. ", defaulting to false)" )
-			consoleOutput = false
-		else
-			consoleOutput = params.consoleOutput or false
-		end
-
-		if params.directory and _type( params.directory ) ~= "userdata" then
-			_print( "WARNING: " .. title .. "bad argument #2 'directory' to 'preload' (userdata expected, got " .. _type( params.directory ) .. ", defaulting to system.ResourceDirectory)" )
-			directory = system.ResourceDirectory
-		else
-			directory = params.directory or system.ResourceDirectory
-		end
-	else
-		-- Default settings if no params table is passed.
-		deepScan, consoleOutput, directory = false, false, system.ResourceDirectory
-	end
+fontLoader.preload = function(...)
+	local t = {...}
+	local folder = type(t[1]) == "string" and t[1] or ""
+	local params = type(t[#t]) == "table" and t[#t] or {}
+	local directory = params.directory or system.ResourceDirectory
 
 	local path = pathForFile( folder, directory )
-	if path then
-		local fontCount, time, file_type, font
+	if not path then
+		print( "ERROR: Spyric Font Loader - folder \"" .. (folder and folder or "") .. "\" not found." )
+	else
+		-- fontCount starts at 2 because it includes native.systemFont and native.systemFontBold.
+		local fontCount, time, filetype, font = 2
+		local deepScan = not not params.deepScan
+		local consoleOutput = not not params.consoleOutput
+		-- A list of subfolders to go through with deepScan.
+		local sub = {}
 
 		if consoleOutput then
-			-- fontCount starts at 2 because native.systemFont and native.systemFontBold will always exist.
-			fontCount, time = 2, _getTimer()
+			time = system.getTimer()
 		end
 
-		local t = {}
-		local function checkFolder( t, path, folder )
-			if consoleOutput then _print( "SEARCHING FOR FONTS IN \"" .. folder .. "/\":" ) end
+		local function checkFolder( sub, path, folder )
+			if consoleOutput then print( "SEARCHING FOR FONTS IN \"" .. folder .. "/\":" ) end
 			local folderEmpty = true
-			t.t = {}
+			local folder = folder .. "/"
+			sub.sub = {}
 
 			for file in lfs.dir( path ) do
 				-- Ignore the current folder and its parent folder.
 				if file ~= "." and file ~= ".." then
-					file_type = file:sub( -4 ):lower() -- use lowercase to capture case sensitive filenames.
-					-- Instead, check for any .otf and .ttf font files.
-					if file_type == ".otf" or file_type == ".ttf" then
-						font = newText( "", 0, 0, folder .. "/" .. file, 12 )
-						remove( font )
-						if consoleOutput then folderEmpty = false; fontCount = fontCount+1; _print( fontCount .. ":\t\"" .. folder .. "/".. file .. "\" preloaded." ) end
+					local filepath = folder .. file
+					filetype = file:sub( -4 ):lower()
+					if filetype == ".otf" or filetype == ".ttf" then
+						font = newText( "", 0, 0, filepath, 12 )
+						dRemove( font )
+						fontCount = fontCount+1
+						if consoleOutput then
+							folderEmpty = false
+							print( fontCount .. ":\t\"" .. filepath .. "\" preloaded." )
+						end
 					else
+						-- With deepScan, check if there are other subfolders inside the current
+						-- directory that also need to be scanned for .otf and .ttf font files.
 						if deepScan then
-							-- If the 4th last symbol in the name of the file/folder is a dot ".", then it is likely not a folder and it will be ignored.
-							-- NB! If you use weird naming conventions and have a folder with a name like "folder.gfx", then said folder would be ignored.
-							if file_type:sub( 1, 1 ) ~= "." then
-								local subfolder = pathForFile( folder .. "/" .. file, system.ResourceDirectory )
-								-- Create a table reference for
-								t.t[#t.t+1] = { subfolder, folder .. "/" .. file }
+							local subfolder = pathForFile( filepath, directory )
+							if lfs.attributes( subfolder, "mode" ) == "directory" then
+								sub.sub[#sub.sub+1] = { subfolder, filepath }
 							end
 						end
 					end
 				end
 			end
 
-			if folderEmpty and consoleOutput then _print( " \t No fonts found." ) end
-			-- If deepScan is enabled, then Loop through all subfolders and load all fonts found inside them.
-			for i = 1, #t.t do
-				if consoleOutput then _print( "" ) end -- Add an empty line after each subfolder.
-				checkFolder( t.t[i], t.t[i][1], t.t[i][2] )
+			if folderEmpty and consoleOutput then print( " \tNo fonts found." ) end
+			-- If deepScan is enabled, then Loop through all newly found subfolders.
+			for i = 1, #sub.sub do
+				if consoleOutput then print( "" ) end
+				local t = sub.sub[i]
+				checkFolder( t, t[1], t[2] )
 			end
 		end
 
 		-- Start by preloading native.systemFont and native.systemFontBold.
 		font = newText( "", 0, 0, native.systemFont, 12 )
-		remove( font )
+		dRemove( font )
 		font = newText( "", 0, 0, native.systemFontBold, 12 )
-		remove( font )
+		dRemove( font )
 
 		if consoleOutput then
-			_print( "-------------------------------------------\n    " .. title .. "www.spyric.com\n-------------------------------------------" )
-			_print( path .. "\n-------------------------------------------\n" )
-			_print( "PRELOADING NATIVE SYSTEM FONTS:" )
-			_print( "1:\tnative.systemFont preloaded." )
-			_print( "2:\tnative.systemFontBold preloaded.\n" )
+			print( "-------------------------------------------\nSpyric Font Loader - https://www.spyric.com\n-------------------------------------------" )
+			print( path .. "\n-------------------------------------------\n" )
+			print( "PRELOADING NATIVE SYSTEM FONTS:" )
+			print( "1:\tnative.systemFont preloaded." )
+			print( "2:\tnative.systemFontBold preloaded.\n" )
 		end
 
-		-- Then load all custom fonts found inside the target folder (and its subfolders).
-		checkFolder( t, path, folder )
+		-- Load all custom fonts found inside the target folder (and its subfolders).
+		checkFolder( sub, path, folder )
 
-		if consoleOutput then _print( "\n-------------------------------------------\n Completed: " .. fontCount .. " fonts preloaded in " .. _getTimer()-time .. "ms\n-------------------------------------------" ) end
-	else
-		_print( "ERROR: " .. title .. "folder \"" .. folder .. "\" not found." )
+		if consoleOutput then
+			print( "\n-------------------------------------------\nCompleted: " .. fontCount .. " fonts preloaded in " .. math.round((system.getTimer()-time)*100)*0.01 .. "ms\n-------------------------------------------" )
+		end
+		return fontCount
 	end
 end
 
-return M
+return fontLoader
